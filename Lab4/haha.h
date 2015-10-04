f808e440ee09aaa5e99adbfbb05732d6bb4a9525
bsudheesh <sudheesh.bhattarai@bison.howard.edu>
2015-10-03 21:52:07 -0400

    Update haha.h

--- Lab4/haha.h
+++ Lab4/haha.h
@@ -3,7 +3,7 @@ using namespace std;
 struct haha
 {
 	int n;
-	haha* link;
+	haha* link; //pointer that connects to its next item in Linked List
 };
 typedef haha* NodePtr;
-void add_elements(NodePtr& head,int number);
+void add_elements(NodePtr& head,int number); //Function used to add elements from the file
