49879d7671f2587867705d1b7f9d95d43124d145
bsudheesh <sudheesh.bhattarai@bison.howard.edu>
2015-10-14 09:24:21 -0400

    Create gradStudent.cpp

---
+++ Labs/Lab-2/gradStudent.cpp
@@ -0,0 +1,10 @@
+#include "gradStudent.h"
+#include <iostream>
+using namespace std;
+void gradStudent::printgradStudent()
+{
+	cout << "Name: " <<	full_name << endl;
+	cout << "ID: "  << id << endl;
+	cout <<  "GPA: " << GPA << endl;
+	cout << "Research Area: " << research << endl;
+}
