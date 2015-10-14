a677ed6d1444eba3178aed4950b26a67d6b9a07a
bsudheesh <sudheesh.bhattarai@bison.howard.edu>
2015-10-14 06:45:25 -0400

    Create department.h

---
+++ Labs/Lab-2/department.h
@@ -0,0 +1,19 @@
+#include<iostream>
+#include "student.h"
+#include "gradStudent.h"
+struct department
+{
+	public:
+		Student stud[15];
+		gradStudent gradstud[15];
+		void printGrad();
+		void printstudent(Student stu[], int num);
+		void sortStudent(Student stu[], int num);
+		void printgradstudent(gradStudent gstu[], int num);
+		void sortgradstudent(gradStudent gstu[], int num);
+
+
+	
+};
+
+
