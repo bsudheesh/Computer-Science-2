bb81ee1f8ac11838291965cb311917cb791632dd
bsudheesh <sudheesh.bhattarai@bison.howard.edu>
2015-10-14 06:46:08 -0400

    Create student.h

---
+++ Labs/Lab-2/student.h
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <string>
+using namespace std;
+class Student
+{
+	public:
+		string fn,ln,t;
+		int id;
+		float GPA;
+		void printStudent();//For printing the members of student
+		//Fuction that initilalizes the name, id and gpa
+		void ini(string n, int i, float g)
+		{
+			t = n;
+			id = i;
+			GPA = g;
+		
+		}
+		//function that returns the name
+		string getName()
+		{
+				return l;
+		}
+		//function that returns the id
+		int getId()
+		{
+			return id;
+		}
+		//function that returns the GPA
+		float getGPA()
+		{
+			return GPA;
+		}
+};
