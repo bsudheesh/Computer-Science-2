9005a345e04435170bd2881c9eef614b26934bf5
bsudheesh <sudheesh.bhattarai@bison.howard.edu>
2015-10-21 16:04:36 -0400

    Create stack.h

---
+++ Labs/Lab5/Part-2/stack.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class stack
+{
+	public:
+			void Ini(string s);
+			bool check_syntax(string s);
+	private:
+			string str;
+			bool answer;
+};
